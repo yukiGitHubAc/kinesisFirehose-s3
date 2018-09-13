7aca4b7ce90d4512a93bb3f096e10d28554a2e4e
yuki.yoshida <yuki.yoshida@bitcoinbank.co.jp>
2018-09-13 10:33:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# kinesisFirehose-s3
+
+# config
+
+Please set variables.
+
+### ./bin/config.ini
+- REGION
+- S3_BUCKET_NAME
+- PATH_TO_OBJECT
+- STACK_NAME
+- ROLE_ARN
+
+# deploy
+
+```bash
+./bin/create-template.bash
+./bin/deploy.bash
+```
---
+++ bin/create-template.bash
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+# ログ設定
+cur_time=$(date +%Y%m%d%H%M%S)
+log_file=./log.$(basename ${0}).${cur_time}
+exec 2> ${log_file}; set -xv
+# ------------------------------------------------------------------------------
+source ./config.ini &> /dev/null
+# ------------------------------------------------------------------------------
+# ネストされたテンプレートをデプロイ
+#aws s3 cp ../src/kinesis-firehose-s3-nested-stack.yaml s3://${S3_BUCKET_NAME}/${PATH_TO_OBJECT}/
+#[ $? -ne 0 ] && exit 1
+# ------------------------------------------------------------------------------
+# テンプレートを作成
+[ ! -d ../dist ] && mkdir ../dist
+rm -rf ../dist/*
+
+cat ../src/kinesis-firehose-s3-root-stack.yaml |
+    sed -e 's@###NESTED_TEMPLATE_YAML###@'https://s3-${REGION}.amazonaws.com/${S3_BUCKET_NAME}/${PATH_TO_OBJECT}/kinesis-firehose-s3-nested-stack.yaml'@g' \
+        -e 's@###BUCKET_ARN###@arn:aws:s3:::'${S3_BUCKET_NAME}'@g'  >> ../dist/kinesis-firehose-s3.package.yaml
+[ $? -ne 0 ] && exit 1
+# ------------------------------------------------------------------------------
+rm -rf ${log_file}
+exit 0
